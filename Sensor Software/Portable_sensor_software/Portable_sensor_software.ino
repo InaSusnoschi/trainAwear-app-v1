343ea59f6d74fbc02690e2cebe1dfd2b51261288
BorkoG <borko_3g@mail.com>
2019-04-06 20:25:18 +0100

    Added initial software skeleton for the sensor microcontroller code. Based on Arduino dev environment and not on pure toolchain.

---
+++ Sensor Software/Portable_sensor_software/Portable_sensor_software.ino
@@ -0,0 +1,90 @@
+/** Software for train-A-wear sensor microcontrollers.
+ *  
+ *  Version:    0.1
+ *  Maintainer: Borko 
+ *  Contacts:   https://github.com/InaSusnoschi/train-A-wear
+ *  Target mCU: ESP8266
+ *  
+ */
+
+#include <ESP8266WiFi.h>
+#include <WiFiUDP.h>
+#include <Ticker.h>
+
+
+#define SERVICE_PORT      31415
+#define ROUTINE_PERIOD_MS 50 
+
+Ticker timedRoutines;
+
+
+// UDP Variables
+WiFiUDP UDP;
+
+
+// Programme variables
+char[] newData;
+
+/**
+ *  A function that polls the sensor over I2C. It updates the 
+ *  existing programme variables with the results, instead of returning them.
+ *  
+ * param:   None
+ * returns: void
+ */
+
+void pollSensor(){
+  
+}
+
+/**
+ * Simple function that collates all the sensor data and turns it into 
+ * the format from the server so it can be transmitted.
+ * 
+ * param:   None
+ * returns: char []
+ */
+
+char[] makePayload(){
+  
+}
+
+/**
+ * A function that takes a payload and transmits it as UDP packet to the master server.
+ * 
+ * param:   char[] payload
+ * returns: void
+ */
+
+void sendUDP(char[] payload){
+  UDP.beginPacket(UDP.remoteIP(), SERVICE_PORT);
+  UDP.write(payload, sizeof(payload));
+  UDP.endPacket();
+}
+
+/** 
+ *  A function that reads sensor data over I2C and transmits it over UDP to the server.
+ *  
+ *  param:    none
+ *  returns:  void
+ */
+
+void readAndTransmit(){
+  pollSensor();
+  newData = makePayload();
+  sendUDP(newData);
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+
+  // Initialises a ticker that calls the routine every so many ms
+  timedRoutines.attach_ms(ROUTINE_PERIOD_MS, readAndTransmit);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
